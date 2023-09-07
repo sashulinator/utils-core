6bff718bbb2df45db38058c4eb6bdf8d3a5e438e
Savchenko Aleksandr <33823497+sashulinator@users.noreply.github.com>
2023-09-07 09:13:09 +0400

    Refactoring 42 (#256)
    * vite-fix-2
    
    * ref(routes) ref
    
    * ref(emitter)
    
    * ref(shared/emitter) add new events
    
    ---------
    
    Co-authored-by: Savchenko Alexander <asavchenko@datatech.ru>

--- index.ts
+++ index.ts
@@ -23,6 +23,7 @@ export * from './types/value-or-setter'
 export * from './types/setter-or-updater'
 
 export * from './c'
+export * from './set-document-title'
 export * from './generate-id'
 export * from './curry'
 export * from './invariant'
