d921a60fdf7a27f49ecca05a09ec93d4335f21e1
Savchenko Alexander <asavchenko@datatech.ru>
2023-10-17 15:50:31 +0400

    some fixes

--- is/dev.ts
+++ is/dev.ts
@@ -4,5 +4,5 @@
  * @return {boolean} `true` if the application is in development, `false` otherwise.
  */
 export function isDev(): boolean {
-  return process.env.NODE_ENV === 'development'
+  return process.env.NODE_ENV === 'development' || localStorage.getItem('env') === 'dev'
 }
