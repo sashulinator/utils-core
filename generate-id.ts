9d185fa3b17d5d950d00bb97cb4d458c1a2feb07
Savchenko Alexander <asavchenko@datatech.ru>
2023-12-04 23:14:11 +0400

    feat(utils) generateUniqId

--- generate-id.ts
+++ generate-id.ts
@@ -5,90 +5,90 @@
  * GitHub page: https://github.com/themimitoof/node-randomid
  *
  */
-
-function randomIntInc(low: number, high: number): number {
-  return Math.floor(Math.random() * (high - low + 1) + low)
-}
+import { random } from './random'
 
 export function generateId(chars = 8): string {
   const key: number[] = []
 
-  var char = [
-    'a',
-    'b',
-    'c',
-    'd',
-    'e',
-    'f',
-    'g',
-    'h',
-    'i',
-    'j',
-    'k',
-    'l',
-    'm',
-    'n',
-    'o',
-    'p',
-    'q',
-    'r',
-    's',
-    't',
-    'u',
-    'v',
-    'w',
-    'x',
-    'y',
-    'z',
-    'A',
-    'B',
-    'C',
-    'D',
-    'E',
-    'F',
-    'G',
-    'H',
-    'I',
-    'J',
-    'k',
-    'L',
-    'M',
-    'N',
-    'O',
-    'P',
-    'Q',
-    'R',
-    'S',
-    'T',
-    'U',
-    'V',
-    'W',
-    'X',
-    'Y',
-    'Z',
-    '0',
-    '1',
-    '2',
-    '3',
-    '4',
-    '5',
-    '6',
-    '7',
-    '8',
-    '9',
-  ]
+  key[0] = random(0, char.length - 1)
 
-  // First character
-  key[0] = randomIntInc(0, char.length - 1)
+  let result = char[key[0]]
 
-  var result = char[key[0]]
-
-  // Lasts characters
-  for (var i = 1; i < chars; i++) {
-    key[i] = randomIntInc(0, char.length - 1)
+  for (let i = 1; i < chars; i++) {
+    key[i] = random(0, char.length - 1)
 
     result += char[key[i]]
   }
 
   return result
 }
+
+export function generateUniqId(chars = 8, isUniq: (id: string) => boolean): string {
+  const result = generateId(chars)
+  return isUniq(result) ? result : generateUniqId(chars, isUniq)
+}
+
+const char = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'k',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
+  '0',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+]
